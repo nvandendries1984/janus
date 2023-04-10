4e7a1b266362da63220b6d49280548f3dbf90dd4
NielsvandenDries <niels@openmindedit.nl>
2023-04-10 13:35:38 +0200

    tijd berekenen

--- models/Uren.php
+++ models/Uren.php
@@ -1,27 +1,44 @@
 <?php namespace NielsVanDenDries\Janus\Models;
 
 use Model;
+use Carbon;
 
-/**
- * Model
- */
 class Uren extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
 
-    /**
-     * @var string The database table used by the model.
-     */
     public $table = 'nielsvandendries_janus_uren';
 
-    /**
-     * @var array Validation rules
-     */
     public $rules = [
     ];
 
     public $belongsTo = [
         'project' => \NielsVanDenDries\Janus\Models\Projecten::class
     ];
+
+    // tijd berekenen
+
+    public function setVanAttribute($value)
+    {
+        $this->attributes['van'] = $value;
+        $this->berekenTijd();
+    }
+
+    public function setTotAttribute($value)
+    {
+        $this->attributes['tot'] = $value;
+        $this->berekenTijd();
+    }
+
+    protected function berekenTijd()
+    {
+        if ($this->van && $this->tot) {
+            $van = \Carbon\Carbon::parse($this->van);
+            $tot = \Carbon\Carbon::parse($this->tot);
+            $verschilInMinuten = $van->diffInMinutes($tot);
+            $this->attributes['bereikte_tijd'] = $verschilInMinuten;
+        }
+    }
+
+    // einde tijd berekenen
 }
