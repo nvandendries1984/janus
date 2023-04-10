a61666c424997a249c5205ad55c93212b6fa82f7
NielsvandenDries <niels@openmindedit.nl>
2023-04-10 14:56:34 +0200

    update

--- updates/builder_table_create_nielsvandendries_janus_betalingen.php
+++ updates/builder_table_create_nielsvandendries_janus_betalingen.php
@@ -13,6 +13,7 @@ class BuilderTableCreateNielsvandendriesJanusBetalingen extends Migration
             $table->increments('id')->unsigned();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
+            $table->string('btw_tarief')->nullable();
             $table->date('factuurdatum');
             $table->decimal('bedrag', 10, 2);
             $table->string('status');
